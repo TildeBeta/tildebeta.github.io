a976aa4621153f40f3d1e483c93e57ea9e130455
TildeBeta <tildebetabot@gmail.com>
2018-03-17 22:47:55 +0200

    Make music skippable

--- assets/scripts/audio.js
+++ assets/scripts/audio.js
@@ -1,19 +1,33 @@
 var player = document.getElementById("audio");
+var previous = null;
 
+var next_button = document.getElementById("next");
+next_button.onclick = next;
+
+var nowplaying = document.getElementById("nowplaying");
+
+
+var songlist = [
+    ["/assets/audio/BatCountry.mp3", "Avenged Sevenfold - Bat Country"],
+    ["/assets/audio/LakeBodom.mp3",  "Children of Bodom - Lake Bodom" ],
+    ["/assets/audio/Megitsune.mp3",  "Babymetal - Megitsune"          ],
+];
 
 function next() {
-    var songlist = [
-        "/assets/audio/BatCountry.mp3",
-        "/assets/audio/LakeBodom.mp3"
-    ];
-    var next_song = songlist[Math.floor(Math.random() * songlist.length)];
-    // TODO prevent choosing the same song?
+    do {
+        var next_song = songlist[Math.floor(Math.random() * songlist.length)];
+    } while (next_song === previous);
+
     play(next_song);
 }
 
 
-function play(name) {
-    player.setAttribute("src", name);
+function play(song) {
+    previous = song;
+
+    nowplaying.innerText = "Now playing: " + song[1];
+
+    player.setAttribute("src", song[0]);
     player.load();
     player.play();
     player.addEventListener("ended", next);
